7c6d1e2ddf770c652e8d688537f461d6c97f5e11
Emdgn <degeneve@kth.se>
2023-06-29 14:30:56 +0200

    Début test réservation

--- src/test/java/com/inti/controller/ReservationControllerTests.java
+++ src/test/java/com/inti/controller/ReservationControllerTests.java
@@ -1,5 +1,9 @@
 package com.inti.controller;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -18,6 +22,11 @@ public class ReservationControllerTests {
 	IReservationRepository irr;
 	
 	
-	
+	@Test
+	public void saveReservation() throws Exception {
+		mockMvc.perform(get("/enregistrerReservation/1"))
+		.andExpect(status().isOk())
+		.andDo(print());
+	}
 
 }
--- src/test/java/com/inti/repository/ReservationRepositoryTests.java
+++ src/test/java/com/inti/repository/ReservationRepositoryTests.java
@@ -1,11 +1,18 @@
 package com.inti.repository;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import com.inti.model.Reservation;
 
 @ExtendWith(SpringExtension.class)
 @DataJpaTest
@@ -14,5 +21,16 @@ public class ReservationRepositoryTests {
 	
 	@Autowired
 	IReservationRepository irr;
+	
+	
+	@Test
+	public void saveReservation() {
+		Reservation reservation = new Reservation(LocalDate.of(2023, 01, 01), 2);
+		
+		Reservation reservationSaved = irr.save(reservation);
+		
+		assertThat(reservationSaved).isNotNull();
+		assertThat(reservationSaved.getIdVoyageur()).isGreaterThan(0);
+	}
 
 }

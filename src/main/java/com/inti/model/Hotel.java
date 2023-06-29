0a9f164ad75d3adcd9e9dd5dc897116fe26bc035
Emdgn <degeneve@kth.se>
2023-06-29 10:09:02 +0200

    Table hotel

---
+++ src/main/java/com/inti/model/Hotel.java
@@ -0,0 +1,22 @@
+package com.inti.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity @Table
+@Data @NoArgsConstructor @AllArgsConstructor
+public class Hotel {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int idHotel;
+	private String nom;
+	private int nbEtoile;
+
+}
